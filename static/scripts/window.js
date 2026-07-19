e4aca72705b1a09f5799724e51a80ea7eceb7b32
Maya Daniel <noahd29@parkschool.net>
2026-07-19 23:02:56 +0000

    refactor: added clearer windowState object

--- static/scripts/window.js
+++ static/scripts/window.js
@@ -1,53 +1,75 @@
-// Bit 1: Window open?
-// Bit 2: Window visible?
+/**
+ * @type {Object.<string, {x: number, y: number, z: number, width: number, height: number, open: boolean, minimized: boolean, maximized: boolean}>}
+ */
 var windowState = {
-    "window1": 0b11,
+    "window1": {
+        x: 0,
+        y: 0,
+        z: 0,
+        width: 0,
+        height: 0,
+        open: true,
+        minimized: false,
+        maximized: false,
+    },
 }
 
+function updateWindows() {
+    for (const [windowID, state] of Object.entries(windowState)) {
+        let window = document.getElementById(windowID);
+        
+        if (state.minimized || !state.open) {
+            window.style.visibility = "hidden";
+        } else {
+            window.style.visibility = "visible";
+        }
+
+        if (state.maximized) {
+            window.style.top = "0px";
+            window.style.left = "0px";
+            window.style.width = "100%";
+            window.style.height = "100%"
+        } else {
+            window.style.left = `${state.x}px`;
+            window.style.top = `${state.y}px`;
+            window.style.width = `${state.width}px`;
+            window.style.height = `${state.height}px`;
+        }
+    }
+}
 
 
 /**
  * @param {number} windowID
  */
 function minimize(windowID) {
-    let window = document.getElementById(windowID);
-    window.setAttribute("style", "visibility: hidden;");
-    windowState[windowID] &= 0b10;
+    windowState[windowID].minimized = false;
+    updateWindows();    
 }
 
 /**
  * @param {number} windowID
  */
 function maximize(windowID) {
-    let window = document.getElementById(windowID);
-
     let button = document.getElementById(windowID + "maximize");
-
-    if (window.classList.contains("maximized")) {
-        window.classList.remove("maximized");
+    
+    if (windowState[windowID].maximized) {
+        windowState[windowID].maximized = false;
         button.innerText = "🗖";
-
-        let originalX = window.getAttribute("originalX") || 0;
-        let originalY = window.getAttribute("originalY") || 0;
-        let originalWidth = window.getAttribute("originalW") || 0;
-        let originalHeight = window.getAttribute("originalH") || 0;
-
-        window.setAttribute("style", `top: ${originalY}px; left: ${originalX}px; width: ${originalWidth}px; height: ${originalHeight}px;`);
     } else {
-        window.classList.add("maximized");
+        windowState[windowID].maximized = true;
         button.innerText = "🗗";
 
-        window.setAttribute("originalX", window.getBoundingClientRect().x || 0);
-        window.setAttribute("originalY", window.getBoundingClientRect().y || 0);
-        window.setAttribute("originalW", window.getBoundingClientRect().width || 0);
-        window.setAttribute("originalH", window.getBoundingClientRect().height || 0);
-
-        window.setAttribute("style", `top: 0px; left: 0px; width: 100%; height: 100%;`);
+        windowState[windowID].x = window.getBoundingClientRect().x || 0;
+        windowState[windowID].y = window.getBoundingClientRect().y || 0;
+        windowState[windowID].width = window.getBoundingClientRect().width || 100;
+        windowState[windowID].height = window.getBoundingClientRect().height || 100;
     }
+
+    updateWindows();
 }
 
 function closeWindow(windowID) {
-    let window = document.getElementById(windowID);
-    window.setAttribute("style", "visibility: hidden;");
-    windowState[windowID] &= 0b10;
+    windowState[windowID].open = false;
+    updateWindows();
 }
